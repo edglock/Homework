5fa27c6d95171531af829eca07168883e35d551f
Edward <edglock@gmail.com>
2020-03-02 15:37:22 +0000

    completed

--- js/index.js
+++ js/index.js
@@ -1,20 +1,22 @@
 document.querySelector('#timeStamp').innerText = new Date().toLocaleTimeString();
-document.querySelector('#get-ajax-html').addEventListener('click', getHtml);
-function getHtml() {
+
+document.querySelector('#get-ajax-html').addEventListener('click', getAjaxHtml);
+function getAjaxHtml() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4 && xhr.status === 200) {
-            document.querySelector('#get-html').innerHTML = xhr.responseText;
+            document.querySelector('#section-html').innerHTML = xhr.responseText;
         }
     }
     xhr.open('GET', 'client-data.html', true);
     xhr.send();
 }
-document.querySelector('#fetch-html').addEventListener('click', fetchHtml);
-function fetchHtml() {
+
+document.querySelector('#fetch-html').addEventListener('click', getFetchHtml);
+function getFetchHtml() {
     fetch('client-data.html')
         .then(response => response.text())
-        .then(html => document.querySelector('#get-html').innerHTML = html);
+        .then(html => document.querySelector('#section-html').innerHTML = html)
 }
 
 document.querySelector('#get-ajax-json').addEventListener('click', getAjaxJson);
@@ -22,20 +24,21 @@ function getAjaxJson() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4 && xhr.status === 200) {
-            const data = JSON.parse(xhr.responseText);
-            document.querySelector('#client-name').innerHTML = data.name;
-            document.querySelector('#client-age').innerHTML = data.age;
+            const clientData = JSON.parse(xhr.responseText);
+            document.querySelector('#client-name').innerHTML = clientData.name;
+            document.querySelector('#client-age').innerHTML = clientData.age;
         }
     }
     xhr.open('GET', 'client-data.json', true);
     xhr.send();
 }
-document.querySelector('#fetch-json').addEventListener('click', fetchJson);
-function fetchJson() {
+
+document.querySelector('#fetch-json').addEventListener('click', getFetchJson);
+function getFetchJson() {
     fetch('client-data.json')
         .then(response => response.json())
-        .then(data => {
-            document.querySelector('#client-name').innerHTML = data.name;
-            document.querySelector('#client-age').innerHTML = data.age;
+        .then(clientData => {
+            document.querySelector('#client-name').innerHTML = clientData.name;
+            document.querySelector('#client-age').innerHTML = clientData.age;
         })
 }
